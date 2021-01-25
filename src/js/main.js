fd0afed8756009f379d120169c3a72cf77e97e34
Erick Aguilar <ericksvaguilar@gmail.com>
2021-01-25 15:20:34 -0300

    finish api handle

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,45 @@
-const key = 'P2bQNbtE3kG4piWBVUcTpUQLLn1DtK2M'
-const city = 'manchester'
-const locationAPIUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`
+const formElement = document.querySelector('[data-js="form"]')
+const weatherCard = document.querySelector('[data-js="weather-card"]')
 
-const forecastAPIUrl = locationKey => `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${key}`
+const endpoint = cityname => `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=aca1524dfb7f6fe280b35f0def53192d`;
 
-const handleResponse = response => response.json()
-
-const handleLocationData = data => {
-  return forecastAPIUrl(data[0].Key)
+const getAPIData = async (cityname) => {
+  try {
+    const response = await fetch(endpoint(cityname))
+    return response.json()
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-const handleError = err => console.error(err)
+const insertDataIntoHTML = (json) => {
+  let temperature = Math.floor(json.main.temp)
+  let cityname = json.name
+  let weatherDescription = json.weather[0].description
+  let iconId = json.weather[0].icon
+  let country = json.sys.country
 
+  weatherCard.innerHTML = `
+    <h2 class="weather-card_city">${cityname} - ${country}</h2>
 
-// || get location information
-fetch(locationAPIUrl)
-  .then(handleResponse)
-  .then(handleLocationData)
-  .then(url => console.log(url))
-  .catch(handleError)
+    <p class="weather-card_temperature">${temperature}°C</p>
+    <figure class="weather-card_figure">
+      <img src="http://openweathermap.org/img/wn/${iconId}@4x.png" alt="${weatherDescription}">
+      <figcaption>${weatherDescription}</figcaption>
+    </figure>
+  
+  `
 
+}
+
+const getData = () => {
+  const formData = new FormData(formElement)
+  const cityname = formData.get('q')
+
+  getAPIData(cityname).then(json => insertDataIntoHTML(json))
+}
+// event listener
+formElement.addEventListener('submit', e => {
+  e.preventDefault()
+  getData()
+})
