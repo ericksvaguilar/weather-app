2aee1477d5d1d7466755a20e8a2d6ee4c15ef652
Erick Aguilar <ericksvaguilar@gmail.com>
2021-01-28 10:33:28 -0300

    refactor main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,46 @@
 import WEATHER_API_KEY from './apikey.js';
-const formElement = document.querySelector('[data-js="form"]')
-const weatherCard = document.querySelector('[data-js="weather-card"]')
+const formElement = document.querySelector('[data-js="form"]');
+const weatherCard = document.querySelector('[data-js="weather-card"]');
 
-const url = cityname => `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=${WEATHER_API_KEY}`;
+const url = cityname =>
+  `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=${WEATHER_API_KEY}`;
 
-const getAPIData = async (cityname) => {
+const getAPIData = async cityname => {
   try {
-    const response = await fetch(url(cityname))
-    return response.json()
+    const response = await fetch(url(cityname));
+    return response.json();
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
-const insertDataIntoHTML = (json) => {
-  let temperature = Math.floor(json.main.temp)
-  let cityname = json.name
-  let weatherDescription = json.weather[0].description
-  let iconId = json.weather[0].icon
-  let country = json.sys.country
+const insertDataIntoHTML = json => {
+  let temperature = Math.floor(json.main.temp);
+  let cityname = json.name;
+  let weatherDescription = json.weather[0].description;
+  let iconId = json.weather[0].icon;
+  let country = json.sys.country;
 
   weatherCard.innerHTML = `
     <h2 class="weather-card_city">${cityname} - ${country}</h2>
 
     <p class="weather-card_temperature">${temperature}°C</p>
+
     <figure class="weather-card_figure">
       <img src="http://openweathermap.org/img/wn/${iconId}@4x.png" alt="${weatherDescription}">
       <figcaption>${weatherDescription}</figcaption>
     </figure>
-  
-  `
-
-}
+  `;
+};
 
 const getData = () => {
-  const formData = new FormData(formElement)
-  const cityname = formData.get('q')
+  const formData = new FormData(formElement);
+  const cityname = formData.get('q');
 
-  getAPIData(cityname).then(json => insertDataIntoHTML(json))
-}
+  getAPIData(cityname).then(json => insertDataIntoHTML(json));
+};
 // event listener
 formElement.addEventListener('submit', e => {
-  e.preventDefault()
-  getData()
-})
+  e.preventDefault();
+  getData();
+});
